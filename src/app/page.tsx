093e6e58e49866374f70d33d428335c08bbcc588
MD Amirul Islam <shadow.actual131@gmail.com>
2025-01-17 02:38:46 +0600

    first changes to check github action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,6 +21,7 @@ export default function Home() {
             .
           </li>
           <li>Save and see your changes instantly.</li>
+          <li>Starting of a fascist story.</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
